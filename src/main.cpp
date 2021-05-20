f0ec52955fab3ed033ccd85ec53bc24a1cc1e2f4
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2021-05-20 15:15:31 +0900

    test relay and photo coupler
    test relay and photo coupler

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,37 @@
 #include <Arduino.h>
 
-uint8_t outpin1 = 4;
-uint8_t outpin2 = 5;
-uint8_t outpin3 = 19;
-uint8_t outpin4 = 18;
+uint8_t relay1 = 4;
+uint8_t relay2 = 5;
+uint8_t photocoupler1 = 19;
+uint8_t photocoupler2 = 18;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(outpin1, OUTPUT);
-  pinMode(outpin2, OUTPUT);
-  pinMode(outpin3, OUTPUT);
-  pinMode(outpin4, OUTPUT);
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(photocoupler1, OUTPUT);
+  pinMode(photocoupler2, OUTPUT);
 
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(outpin1, LOW);
-  digitalWrite(outpin2, LOW);
-  digitalWrite(outpin3, LOW);
-  digitalWrite(outpin4, LOW);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, LOW);
+  digitalWrite(photocoupler1, LOW);
+  digitalWrite(photocoupler2, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, LOW);
-  //digitalWrite(outpin1, LOW);
-  //digitalWrite(outpin2, LOW);
-  digitalWrite(outpin3, LOW);
-  digitalWrite(outpin4, LOW);
+  //digitalWrite(relay1, LOW);
+  //digitalWrite(relay2, LOW);
+  digitalWrite(photocoupler1, LOW);
+  digitalWrite(photocoupler2, LOW);
   delay(5000);
   digitalWrite(LED_BUILTIN, HIGH);
-  //digitalWrite(outpin1, HIGH);
-  //digitalWrite(outpin2, HIGH);
-  digitalWrite(outpin3, HIGH);
-  digitalWrite(outpin4, HIGH);
+  //digitalWrite(relay1, HIGH);
+  //digitalWrite(relay2, HIGH);
+  digitalWrite(photocoupler1, HIGH);
+  digitalWrite(photocoupler2, HIGH);
   delay(5000);
 }
